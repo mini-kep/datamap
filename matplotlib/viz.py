bb1f54166bcbc393eea393388bf87b80da79acf3
=epogrebnyak <e.pogrebnyak@gmail.com>
2017-11-02 21:33:40 +0300

    get_dataframe() added

--- matplotlib/viz.py
+++ matplotlib/viz.py
@@ -10,7 +10,7 @@ def make_url(freq, name):
            f'datapoints?name={name}&freq={freq}')
 
 
-def read_ts_from_url(source_url):
+def read_from_url(source_url):
 	"""Read pandas time series from *source_url*."""
 	return pd.read_csv(source_url, 
                        converters={0: pd.to_datetime}, 
@@ -19,8 +19,17 @@ def read_ts_from_url(source_url):
 
 def get_ts(freq, name):
     url = make_url(freq, name)
-    return read_ts_from_url(url)
+    return read_from_url(url)
 
+def get_df(freq, *names):
+    df_list = [get_ts(freq, name).to_frame() for name in names]
+    df = df_list[0]
+    for right_df in df_list[1:]:
+       df = df.merge(right_df, right_index=True, left_index=True)
+    return df  
+
+df = get_df('a', 'GDP_yoy', 'CPI_rog')  
+assert isinstance(df, pd.DataFrame)
 
 start = datetime.date(1998, 12, 31)
 end = datetime.date(2017, 12, 31)
